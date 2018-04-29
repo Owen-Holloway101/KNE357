1747e53a19fe3c8957a396a6bf56a9909c667661
Owen Holloway <owen.holloway101@gmail.com>
2018-04-29 12:21:39 +1000

    added location

--- vision.py
+++ vision.py
@@ -10,8 +10,8 @@ cap.set(3,1080)
 cap.set(4,720)
 
 #colour defs
-greenLower = np.array([82, 42, 19],np.uint8)
-greenUpper = np.array([215, 204, 204],np.uint8)
+greenLower = np.array([0, 0, 62],np.uint8)
+greenUpper = np.array([180, 180, 180],np.uint8)
 
 yellowLower = np.array([29, 86, 6],np.uint8)
 yellowUpper = np.array([229, 255, 100],np.uint8)
@@ -22,10 +22,9 @@ while(True):
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    FILTER_GREEN = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FILTER_YELLOW = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    mask_green = cv2.inRange(FILTER_GREEN, greenLower, greenUpper)
+    mask_green = cv2.inRange(frame, greenLower, greenUpper)
     mask_green = cv2.erode(mask_green, None, iterations=2)
     mask_green = cv2.dilate(mask_green, None, iterations=2)
 
@@ -42,10 +41,24 @@ while(True):
         c = max(cntsgreen, key=cv2.contourArea)
         M = cv2.moments(c)
         #print(M)
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cntsgreen, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
 
     # Display the resulting frame
-    cv2.imshow('frame',mask_green)
+    cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
